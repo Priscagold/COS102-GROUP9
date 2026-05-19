2fd36d01fd00eed36cb6ea6aac67697d296bce85
Priscagold <oluwasemilore.olamiju@pau.edu.ng>
2026-05-19 14:39:24 +0100

    Created multi-language translation dictionary module.

---
+++ translations.py
@@ -0,0 +1,37 @@
+UI_STRINGS = {
+    "English": {
+        "welcome": "Welcome to Swift Shop",
+        "market_title": "Marketplace / Product Catalog",
+        "checkout": "Proceed to Checkout",
+        "item_added": "Item successfully added to your market basket!",
+        "guide": "Guide: Click any language button to translate the entire page. Click a product to buy it."
+    },
+    "Yoruba": {
+        "welcome": "Kábọ̀ sí Swift Shop",
+        "market_title": "Ojú-Oge Ọjà / Àwọn Ọjà Wa",
+        "checkout": "Sísanwó / Parí Ìrajà",
+        "item_added": "A ti fi ọjà sínú agbọ̀n tìrẹ lọ́nà àṣeyọrí!",
+        "guide": "Ìtọ́sọ́nà: Tẹ bọ́tìnì èdè kankan láti yí gbogbo ojú-ewé padà. Tẹ ọjà kan láti rà á."
+    },
+    "Igbo": {
+        "welcome": "Nnọọ na Swift Shop",
+        "market_title": "Ebe Ngosipụta Ngwaahịa / Katọlọgụ",
+        "checkout": "Gaa n'Ebe Ịkwụ Ụgwọ",
+        "item_added": "Agbakwunyere ihe a nke ọma na nkata ahịa gị!",
+        "guide": "Ntuziaka: Pịa bọtịnụ asụsụ ọ bụla ka ị sụgharịa ibe ahụ dum. Pịa ngwaahịa ka ị zụọ ya."
+    },
+    "Hausa": {
+        "welcome": "Barka da zuwa Swift Shop",
+        "market_title": "Dandalin Kasuwa / Kayayyakinmu",
+        "checkout": "Ci gaba da Biyan Kuɗi",
+        "item_added": "An yi nasarar ƙara kaya a rariyar sayayya taka!",
+        "guide": "Jagora: Danna kowane bọtini na yare don fassara dukkan shafin. Danna samfur don saya."
+    },
+    "Pidgin": {
+        "welcome": "Welcome to Swift Shop",
+        "market_title": "Market Area / All Beta Market",
+        "checkout": "Go Pay Money for Counter",
+        "item_added": "Market don enter your basket smoothly!",
+        "guide": "Guide: Touch any language button to change all the words. Click the market item to buy am."
+    }
+}
